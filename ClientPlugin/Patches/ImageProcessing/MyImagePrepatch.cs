e9d05999a1c4929e6e3de7ddd8c6384f4388dc4e
Viktor Ferenczi <viktor@ferenczi.eu>
2025-12-24 03:54:57 +0100

    Metadata fixed

--- ClientPlugin/Patches/ImageProcessing/MyImagePrepatch.cs
+++ ClientPlugin/Patches/ImageProcessing/MyImagePrepatch.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using ClientPlugin.Tools;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using VRageRender;
 
 namespace ClientPlugin.Patches.ImageProcessing;
 
@@ -81,18 +82,132 @@ public static class MyImagePrepatch
         var il = method.Body.Instructions;
         il.RecordOriginalCode(method);
 
-        var scope = module.AssemblyReferences.First(r => r.Name == "SixLabors.ImageSharp");
-        SixLaborsHelpers.ReplacePixelFormats(module, scope, il);
+        var sixLaborsImageSharpScope = module.AssemblyReferences.First(r => r.Name == "SixLabors.ImageSharp");
+        SixLaborsHelpers.ReplacePixelFormats(module, sixLaborsImageSharpScope, il);
 
-        // Empty the method, so it can be replaced by a transpiler
-        // Keep only: ldnull, ret
-        var instructions = method.Body.Instructions;
-        var count = instructions.Count;
-        var ldnull = instructions[count - 2];
-        var ret = instructions[count - 1];
-        instructions.Clear();
-        instructions.Add(ldnull);
-        instructions.Add(ret);
+        // Replace this code line:
+        // - oneChannel = imageInfo.MetaData.GetFormatMetaData(PngFormat.Instance).ColorType == PngColorType.Grayscale;
+        // + oneChannel = imageInfo.Metadata.GetPngMetadata().ColorType == PngColorType.Grayscale;
+        //
+        // Original IL:
+        // IL_0012: ldloc.0
+        // IL_0013: callvirt instance class [SixLabors.ImageSharp]SixLabors.ImageSharp.MetaData.ImageMetaData [SixLabors.ImageSharp]SixLabors.ImageSharp.IImageInfo::get_MetaData()
+        // IL_0018: call class [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngFormat [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngFormat::get_Instance()
+        // IL_001d: callvirt instance !!0 [SixLabors.ImageSharp]SixLabors.ImageSharp.MetaData.ImageMetaData::GetFormatMetaData<class [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngMetaData>(class [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.IImageFormat`1<!!0>)
+        // IL_0022: callvirt instance valuetype [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngColorType [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngMetaData::get_ColorType()
+        // IL_0027: ldc.i4.0
+        // IL_0028: ceq
+        // IL_002a: starg.s oneChannel
+        //
+        // Replacement IL:
+        // IL_001a: ldloc.0      // imageInfo
+        // IL_001b: callvirt     instance class [SixLabors.ImageSharp]SixLabors.ImageSharp.Metadata.ImageMetadata [SixLabors.ImageSharp]SixLabors.ImageSharp.ImageInfo::get_Metadata()
+        // IL_0020: call         class [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngMetadata [SixLabors.ImageSharp]SixLabors.ImageSharp.MetadataExtensions::GetPngMetadata(class [SixLabors.ImageSharp]SixLabors.ImageSharp.Metadata.ImageMetadata)
+        // IL_0025: callvirt     instance valuetype [System.Runtime]System.Nullable`1<valuetype [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngColorType> [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngMetadata::get_ColorType()
+        // IL_002a: stloc.2      // V_2
+        // IL_002b: ldc.i4.0
+        // IL_002c: stloc.3      // V_3
+        // IL_002d: ldloca.s     V_2
+        // IL_002f: call         instance !0/*valuetype [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngColorType*/ valuetype [System.Runtime]System.Nullable`1<valuetype [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngColorType>::GetValueOrDefault()
+        // IL_0034: ldloc.3      // V_3
+        // IL_0035: ceq
+        // IL_0037: ldloca.s     V_2
+        // IL_0039: call         instance bool valuetype [System.Runtime]System.Nullable`1<valuetype [SixLabors.ImageSharp]SixLabors.ImageSharp.Formats.Png.PngColorType>::get_HasValue()
+        // IL_003e: and
+        // IL_003f: starg.s      oneChannel
+        var i = il.FindFirstIndex(i => i.OpCode == OpCodes.Callvirt && i.Operand is MethodReference mr && mr.DeclaringType.Name == "ImageInfo" && mr.Name == "get_MetaData");
+        Debug.Assert(i != -1, "Could not find the use of get_MetaData");
+        
+        // Remove original block
+        i -= 1;
+        for(var j = 0; j < 8; j++)
+            il.RemoveAt(i);
+        
+        // Insert replacement block
+        var start = i;
+        
+        // Create type references
+        var imageInfoType = module.ImportReference(new TypeReference("SixLabors.ImageSharp", "ImageInfo", module, sixLaborsImageSharpScope, false));
+        var imageMetadataType = module.ImportReference(new TypeReference("SixLabors.ImageSharp.Metadata", "ImageMetadata", module, sixLaborsImageSharpScope, false));
+        var metadataExtensionsType = module.ImportReference(new TypeReference("SixLabors.ImageSharp", "MetadataExtensions", module, sixLaborsImageSharpScope, false));
+        var pngMetadataType = module.ImportReference(new TypeReference("SixLabors.ImageSharp.Formats.Png", "PngMetadata", module, sixLaborsImageSharpScope, false));
+        var pngColorTypeType = module.ImportReference(new TypeReference("SixLabors.ImageSharp.Formats.Png", "PngColorType", module, sixLaborsImageSharpScope, true));
+        var systemRuntimeScope = module.AssemblyReferences.FirstOrDefault(r => r.Name == "System.Runtime") 
+                               ?? module.AssemblyReferences.First(r => r.Name == "netstandard");
+        var nullableTypeRef = module.ImportReference(new TypeReference("System", "Nullable`1", module, systemRuntimeScope, true));
+        var nullablePngColorType = new GenericInstanceType(nullableTypeRef);
+        nullablePngColorType.GenericArguments.Add(pngColorTypeType);
+        
+        // Create method references with proper return types
+        var getMetadataMethod = new MethodReference("get_Metadata", imageMetadataType, imageInfoType) { HasThis = true };
+        var getPngMetadataMethod = new MethodReference("GetPngMetadata", pngMetadataType, metadataExtensionsType);
+        getPngMetadataMethod.Parameters.Add(new ParameterDefinition(imageMetadataType));
+        var getColorTypeMethod = new MethodReference("get_ColorType", nullablePngColorType, pngMetadataType) { HasThis = true };
+        
+        // Insert IL instructions
+        il.Insert(i++, Instruction.Create(OpCodes.Ldloc_0));
+        il.Insert(i++, Instruction.Create(OpCodes.Callvirt, module.ImportReference(getMetadataMethod)));
+        il.Insert(i++, Instruction.Create(OpCodes.Call, module.ImportReference(getPngMetadataMethod)));
+        il.Insert(i++, Instruction.Create(OpCodes.Callvirt, module.ImportReference(getColorTypeMethod)));
+        il.Insert(i++, Instruction.Create(OpCodes.Stloc_2));
+        il.Insert(i++, Instruction.Create(OpCodes.Ldc_I4_0));
+        il.Insert(i++, Instruction.Create(OpCodes.Stloc_3));
+        il.Insert(i++, Instruction.Create(OpCodes.Ldloca_S, method.Body.Variables[2]));
+        var getValueOrDefaultMethod = new MethodReference("GetValueOrDefault", pngColorTypeType, nullablePngColorType) { HasThis = true };
+        il.Insert(i++, Instruction.Create(OpCodes.Call, module.ImportReference(getValueOrDefaultMethod)));
+        il.Insert(i++, Instruction.Create(OpCodes.Ldloc_3));
+        il.Insert(i++, Instruction.Create(OpCodes.Ceq));
+        il.Insert(i++, Instruction.Create(OpCodes.Ldloca_S, method.Body.Variables[2]));
+        var getHasValueMethod = new MethodReference("get_HasValue", module.TypeSystem.Boolean, nullablePngColorType) { HasThis = true };
+        il.Insert(i++, Instruction.Create(OpCodes.Call, module.ImportReference(getHasValueMethod)));
+        il.Insert(i++, Instruction.Create(OpCodes.And));
+        il.Insert(i++, Instruction.Create(OpCodes.Starg_S, method.Parameters.First(p => p.Name == "oneChannel")));
+        var size = i - start;
+        Debug.Assert(size == 15, $"Wrong replacement block size of {size}, should be 15");
+
+        // First, fix any remaining get_MetaData calls (old API with uppercase 'D') to get_Metadata (new API with lowercase 'd')
+        // This must be done BEFORE looking for the GetFormatMetaData pattern
+        var newMetadataGetter = new MethodReference("get_Metadata", imageMetadataType, imageInfoType) { HasThis = true };
+        foreach (var ins in il)
+        {
+            if (ins.OpCode == OpCodes.Callvirt && ins.Operand is MethodReference mr && mr.Name == "get_MetaData")
+                ins.Operand = module.ImportReference(newMetadataGetter);
+        }
+
+        // AI generated:
+        // Now fix the second occurrence: Replace get_Metadata().GetFormatMetaData(PngFormat.Instance) with get_Metadata().GetPngMetadata()
+        // This pattern appears later in the method and needs to be replaced
+        // Original pattern (3 instructions):
+        //   callvirt get_Metadata()
+        //   call PngFormat.get_Instance()
+        //   callvirt GetFormatMetaData<PngMetaData>()
+        // Replacement (2 instructions):
+        //   callvirt get_Metadata()
+        //   call GetPngMetadata()
+        
+        i = il.FindFirstIndex(i => 
+            i.OpCode == OpCodes.Call && 
+            i.Operand is MethodReference mr && 
+            mr.DeclaringType.Name == "PngFormat" && 
+            mr.Name == "get_Instance");
+        
+        if (i != -1)
+        {
+            // Verify the pattern: should have get_Metadata before and GetFormatMetaData after
+            Debug.Assert(i > 0 && il[i - 1].OpCode == OpCodes.Callvirt && 
+                        il[i - 1].Operand is MethodReference mr1 && mr1.Name == "get_Metadata",
+                        "Expected get_Metadata call before get_Instance");
+            Debug.Assert(i + 1 < il.Count && il[i + 1].OpCode == OpCodes.Callvirt &&
+                        il[i + 1].Operand is MethodReference mr2 && mr2.Name == "GetFormatMetaData",
+                        "Expected GetFormatMetaData call after get_Instance");
+            
+            // Remove the get_Instance and GetFormatMetaData calls (2 instructions)
+            il.RemoveAt(i); // Remove get_Instance
+            il.RemoveAt(i); // Remove GetFormatMetaData (now at position i)
+            
+            // Insert GetPngMetadata call
+            il.Insert(i, Instruction.Create(OpCodes.Call, module.ImportReference(getPngMetadataMethod)));
+        }
         
         il.RecordPatchedCode(method);
     }
@@ -109,6 +224,7 @@ public static class MyImagePrepatch
 
         il.RecordOriginalCode(method);
 
+        // AI generated:
         // Find GetPixelSpan<TImage> call and replace with GetPixelMemoryGroup().Single().Span
         // Original: call valuetype [System.Memory]System.Span`1<!!0> [SixLabors.ImageSharp]SixLabors.ImageSharp.Advanced.AdvancedImageExtensions::GetPixelSpan<!!TImage>(...)
         for (var i = 0; i < il.Count; i++)
