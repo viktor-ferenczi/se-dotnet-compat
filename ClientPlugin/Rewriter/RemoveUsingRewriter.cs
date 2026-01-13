0a9371fd130163e4242b04490516c4698d53426f
Space <unclearspace@gmail.com>
2026-01-13 12:15:05 +0000

    Blacklist more usings

--- ClientPlugin/Rewriter/RemoveUsingRewriter.cs
+++ ClientPlugin/Rewriter/RemoveUsingRewriter.cs
@@ -6,10 +6,11 @@ namespace ClientPlugin.Rewriter;
 
 internal sealed class RemoveUsingRewriter : CSharpSyntaxRewriter
 {
-    internal readonly string[] blacklist = ["System.Deployment"];
+    internal readonly string[] blacklist = ["System.Deployment", "System.Runtime.Remoting.Lifetime", "System.Runtime.Remoting.Messaging", "Microsoft.VisualBasic"];
 
     public override SyntaxNode VisitUsingDirective(UsingDirectiveSyntax node)
     {
+        // FIXME: Try and do this without a hardcoded blacklist
         foreach (var item in blacklist)
             if (node.Name?.ToString().Contains(item) ?? false)
                 return null;
