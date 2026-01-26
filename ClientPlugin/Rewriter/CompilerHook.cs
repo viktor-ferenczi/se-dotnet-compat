af15308d086950378cfe33f6a7eee1e437443b8d
Viktor Ferenczi <viktor@ferenczi.eu>
2026-01-26 03:59:51 +0100

    Fixed compiling mods (#1)
    * Remove bad usings
    * Find conflicting extensions
    * Replace conflicting extensions
    * Use overloads for ExtensionCollector
    * Blacklist more usings
    * Fix Enumerable.FirstOrDefault
    * Automatic invalid using removal
    * Fix Vector3 clash
    * Fix partial mod compiling
    
    Fully authored-by: Space <unclearspace@gmail.com>

---
+++ ClientPlugin/Rewriter/CompilerHook.cs
@@ -0,0 +1,73 @@
+﻿using ClientPlugin.Rewriter;
+using HarmonyLib;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRage.Scripting;
+
+[HarmonyPatchCategory("Init")]
+[HarmonyPatch(typeof(MyScriptCompiler), "CreateCompilation")]
+static class CreateCompilation_Prefix
+{
+    // Copied from Keen. TODO: Replace with a transpiler on the Select
+    public static bool Prefix(
+        MyScriptCompiler __instance,
+        string assemblyFileName,
+        IEnumerable<Script> scripts,
+        bool enableDebugInformation,
+        ref CSharpCompilation __result)
+    {
+        CSharpCompilationOptions options = ((enableDebugInformation || __instance.EnableDebugInformation) ? __instance.m_debugCompilationOptions : __instance.m_runtimeCompilationOptions);
+        IEnumerable<SyntaxTree> syntaxTrees = null;
+        if (scripts != null)
+        {
+            CSharpParseOptions parseOptions = __instance.m_conditionalParseOptions.WithPreprocessorSymbols(__instance.m_conditionalCompilationSymbols);
+            syntaxTrees = GetRewrittenTrees(__instance, [.. scripts], parseOptions, options);
+        }
+        __result = CSharpCompilation.Create(MyScriptCompiler.MakeAssemblyName(assemblyFileName), syntaxTrees, __instance.m_metadataReferences, options);
+        return false;
+    }
+
+    public static IEnumerable<SyntaxTree> GetRewrittenTrees(MyScriptCompiler __instance, List<Script> scripts, CSharpParseOptions parseOptions, CSharpCompilationOptions options)
+    {
+        List<SyntaxTree> initialTrees = [.. scripts.Select((Script s) => CSharpSyntaxTree.ParseText(s.Code, parseOptions, s.Name, Encoding.UTF8))];
+        var analysisCompilation = CSharpCompilation.Create("compat-analysis-compilation", initialTrees, __instance.m_metadataReferences, options);
+
+        // Prevent rewriter edge cases from affecting mods who's compilation succeeded
+        if (!analysisCompilation.GetDiagnostics().Any(d => d.Severity == DiagnosticSeverity.Error))
+            return initialTrees;
+
+        Dictionary<SyntaxTree, SemanticModel> initialTreeInfo = initialTrees.ToDictionary(tree => tree, tree => analysisCompilation.GetSemanticModel(tree));
+
+        // Scan for clashing extension methods
+        Dictionary<IMethodSymbol, List<IMethodSymbol>> conflicts = [];
+        foreach (var tree in initialTrees)
+        {
+            var model = initialTreeInfo[tree];
+            var collector = new ConflictingExtensionCollector(model, conflicts);
+            collector.Visit(tree.GetRoot());
+        }
+
+        // These will replace the tree objects and break the above dictionaries
+        // ORDER OF EXECUTION MATTERS!
+
+        // Fix clashing extension methods
+        initialTrees = [.. initialTrees.Select(tree => {
+            var model = initialTreeInfo[tree];
+            var rewriter = new ConflictingExtensionRewriter(model, conflicts);
+            var newRoot = rewriter.Visit(tree.GetRoot());
+            return CSharpSyntaxTree.Create((CSharpSyntaxNode)newRoot, parseOptions, tree.FilePath, Encoding.UTF8);;
+        })];
+
+    //    System.IO.File.WriteAllText(
+    //System.IO.Path.Combine(
+    //    System.Environment.GetFolderPath(
+    //        System.Environment.SpecialFolder.UserProfile),
+    //    "Downloads\\dump.txt"),
+    //initialTrees.Where(x=>x.FilePath.Contains("SettingsMenu.cs") && x.FilePath.Contains("Input")).First().GetRoot().ToString());
+
+        return initialTrees;
+    }
+}
---
+++ ClientPlugin/Rewriter/ConflictingExtensionCollector.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ClientPlugin.Rewriter;
+
+
+internal sealed class ConflictingExtensionCollector(SemanticModel _semanticModel, Dictionary<IMethodSymbol, List<IMethodSymbol>> conflicts) : CSharpSyntaxWalker
+{
+    public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+        var symbol = _semanticModel.GetDeclaredSymbol(node);
+        if (symbol is not { IsExtensionMethod: true })
+            return;
+
+        var extendedType = symbol.Parameters[0].Type as INamedTypeSymbol;
+        if (extendedType == null)
+            return;
+
+        // FIXME: Use full method signature with types in this
+        var conflict =
+            extendedType.GetMembers(symbol.Name)
+                        .OfType<IMethodSymbol>()
+                        .FirstOrDefault(m => m.IsStatic && IsValidOverload(m, symbol));
+
+        if (conflict is null)
+            return;
+
+        if (conflicts.TryGetValue(conflict, out var possibleExtensions))
+            possibleExtensions.Add(symbol);
+        else
+            conflicts.Add(conflict, [symbol]);
+    }
+
+    private bool IsValidOverload(IMethodSymbol instanceMethod, IMethodSymbol extension)
+    {
+        // Checks call signature of both methods.
+        // Optional params must match if there is a corresponding param on the other function (even if the other param is optional)
+        // Otherwise we ignore them (as there is the case they were not passed)
+        // This is slightly aggressive but best we can do without checking all call sites for optional param usage.
+
+        var instanceParams = instanceMethod.Parameters;
+        var extensionParams = extension.Parameters.Skip(1).ToArray();
+
+        for (int i = 0; i < Math.Max(instanceParams.Length, extensionParams.Length); i++)
+        {
+            if (i >= instanceParams.Length && !extensionParams[i].IsOptional)
+                return false;
+
+            if (i >= extensionParams.Length && !instanceParams[i].IsOptional)
+                return false;
+
+            var extType = extensionParams[i].Type;
+            var instanceType = instanceParams[i].Type;
+
+            var conversion = _semanticModel.Compilation
+                .ClassifyConversion(extType, instanceType);
+
+            if (!conversion.IsImplicit)
+                return false;
+        }
+
+        return true;
+    }
+}
